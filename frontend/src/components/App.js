76e9e630c1fdfdbf39b70606aef7790de690bfcc
u1810291 <otabekbutcher@gmail.com>
2020-12-13 17:25:46 +0500

    another

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import Container from "./Container/Container";
 class App extends Component {
   render() {
     return (
       <div>
         <pre>Something </pre>
         Hello
+        <Container />
       </div>
     );
   }
